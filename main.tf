201c26436e739f2a76b44fd5d4f10e28355b5be2
aduabgy <aduabgy@yahoo.com>
2023-07-25 09:07:35 +0100

    public key added to allow ssh

--- main.tf
+++ main.tf
@@ -1,51 +1,30 @@
 provider "aws"{
   region = "eu-west-2"  
 }
+variable vpc_cidr_block{}
+variable subnet_cidr_block{}
+variable avail_zone{}
+variable env_prefix {}
+variable my_ip{}
+variable instance_type{}
+variable my_public{}
+#variable public_key_location{}
 
 resource "aws_vpc" "my-app-vpc" {
-  cidr_block = var.cidr_blocks[0]
+  cidr_block = var.vpc_cidr_block
   tags = {
-    Name = var.cidr_blocks[0]
-    vpc_env = "dev"
+    Name = "${var.env_prefix}-vpc"
   }
 }
-
-variable "environment" {
-  description = "deployment environment"
-  default = "My-app-dev-VPC"
-}
-
-variable "cidr_blocks" {
-  description = "cidr blocks for vpc and subnets"
-  type = list(string)
-}
-
-
-/* variable "cidr_blocks" {
-  description = "cidr blocks for vpc and subnets"
-  type = list(object({
-    cidr_block = string
-    name = string
-  }))
-} */
 
 resource "aws_subnet" "my-app-subnet-1" {
   vpc_id = aws_vpc.my-app-vpc.id
-  cidr_block = var.cidr_blocks[1]
-  availability_zone = "eu-west-2a"
+  cidr_block = var.subnet_cidr_block
+  availability_zone = var.avail_zone
   tags = {
-    Name = "subnet-1-dev"
+    Name = "${var.env_prefix}-subnet-1"
   }
 }
-
- /* resource "aws_subnet" "my-app-subnet-1" {
-  vpc_id = aws_vpc.my-app-vpc.id
-  cidr_block = var.cidr_blocks[1].cidr_block
-  availability_zone = "eu-west-2a"
-  tags = {
-    Name = var.cidr_blocks[1].cidr_block.name
-  }
-} */
 
 output "dev-vpc-id" {
   value = aws_vpc.my-app-vpc.id
@@ -53,4 +32,108 @@ output "dev-vpc-id" {
 
 output "dev-subnet-id" {
   value = aws_subnet.my-app-subnet-1.id
+}
+
+
+resource "aws_internet_gateway" "myapp-igw" {
+  vpc_id = aws_vpc.my-app-vpc.id
+  tags={
+    Name = "${var.env_prefix}-igw" 
+  }
+}
+resource "aws_route_table" "myapp-route-table" {
+  vpc_id = aws_vpc.my-app-vpc.id 
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myapp-igw.id
+  }
+  tags = {
+    Name = "${var.env_prefix}-rt"
+  }
+}
+
+resource "aws_route_table_association" "a-rt-subnet" {
+  subnet_id = aws_subnet.my-app-subnet-1.id
+  route_table_id = aws_route_table.myapp-route-table.id
+}
+
+resource "aws_security_group" "myapp-sg" {
+  name = "myapp-sg"
+  vpc_id = aws_vpc.my-app-vpc.id
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks =  [ "0.0.0.0/0" ]   #[ var.my_ip ]
+  }
+
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
+
+  egress {
+   from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = [ "0.0.0.0/0" ] 
+  }
+  tags = {
+    Name = "${var.env_prefix}-sg"
+  }
+}
+
+  /* data "aws_ami" "latest-amazon-linux-image" {
+  most_recent = true
+  owners      = ["137112412989"]
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-kernel-5.10-hvm-*-x86_64-gp2"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+ 
+
+
+resource "aws_instance" "myapp-server" {
+   ami = data.aws_ami.latest-amazon-linux-image.id
+   instance_type = var.instance_type
+
+   subnet_id = aws_subnet.my-app-subnet-1.id
+   security_groups = [aws_security_group.myapp-sg.id]
+   availability_zone = var.avail_zone
+
+   associate_public_ip_address = true
+   key_name = "class29"
+
+   tags = {
+    Name = "${var.env_prefix}-server"
+  }
+
+
+}  */
+
+resource "aws_key_pair" "ssh-key" {
+  key_name = "server-key"
+  public_key = var.my_public#"${file(var.public_key_location)}"
+}
+
+resource "aws_instance" "app-server" {
+  ami = "ami-0b026d11830afcbac"
+  instance_type = var.instance_type
+  subnet_id = aws_subnet.my-app-subnet-1.id
+  security_groups = [ aws_security_group.myapp-sg.id ]
+  availability_zone = var.avail_zone
+  associate_public_ip_address = true
+  key_name = aws_key_pair.ssh-key.key_name
+
+   tags = {
+    Name = "${var.env_prefix}-EC2-server"
+  }
 }
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,7 +1,8 @@
-cidr_blocks = ["10.0.0.0/16", "10.0.50.0/24"]
-environment = "Dev-VPC"
-
-/* cidr_blocks = [
-    {cidr_block = 10.0.0.0/16, name = "dev-vpc"},
-    {cidr_block = 10.0.40.0/24, name = "dev-subnet"}
-] */
+vpc_cidr_block = "10.0.0.0/16"
+subnet_cidr_block = "10.0.10.0/24"
+avail_zone = "eu-west-2a"
+env_prefix = "dev"
+my_ip = "94.196.221.60/32"
+instance_type = "t2.micro"
+my_public = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDTSwnROm0q5w8d0U9DnxHGNb8e466Iye6G6KRqLudw2vAT4IqRWdl+1DOUIhVBrbuWDReQtNjjuOw5rVjs9YzVkQ9lJ6iVXj9kuXOMCsyTEI+vn2W/sa6Yl1CviIXTaH94EL4yDnRqiM2SwdR7AeGBP63dxg2KWq5NDQamKwY4Hcm/fiLXoE3eEhaj/QNcyxnwWPN2uhQ7YavWFMSCgm6wrTxHxYZYJOOXmZL6J/hTO0DU+Sxx7p4Wmeqh1O+xOPdu/UtQRv/OBSC/6zs8IX0ClnMp7r+Fu03TJvITTBx5OpQx/XEsPvopzW1Qm/EheJUDC5sfKcwOwP8XO0osihBcGS4xJQcfAwL56uN7LaN13CRxfHeNKzXTMb8i9Xw/0oQmw5YcqUOp2pKuIwaMfejfnE1GTYVtBssUzedJrQsSVQPICqIKHVXnyOJQwMqMMZjZBbtT6sVRupHaKFTcfuAsH1MkyxIqvseNR/bHMesk4Zn40eXpmH91wtsFP6HUPR8= yawad@Adu"
+#public_key_location = "/Users/yawad/.ssh/id_rsa.pub"
